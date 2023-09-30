9d0436fd4195d3ecd8e77d5b9f4436ce208331ee
Валерия Йовхомищ <jovhomislera7@gmail.com>
2023-09-30 11:23:55 +0300

    new

--- tokenjovhomis.pas
+++ tokenjovhomis.pas
@@ -1,1 +1,13 @@
-﻿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
+﻿begin
+  var a:= readinteger;
+  assert(a>=0);
+  assert(a<=23);
+  if (a>=4) and (a<=10) then
+    write('Доброе утро, мир!');
+  if (a>=11) and (a<=16) then
+    write('Добрый день, мир!');
+  if (a>=17) and (a<=22) then
+    write('Добрый вечер, мир!');
+  if (a=23) or ((a>=0) and (a<=3)) then
+    write('Доброй ночи, мир!')
+end.
